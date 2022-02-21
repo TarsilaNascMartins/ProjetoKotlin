3835a816cc8d3c0ba6705ade7a2be2e310c6dac7
Tarsila Nascimento Martins <tarsilawho@gmail.com>
2022-02-21 11:14:52 -0300

    novoKotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    println("Anderson:- Bom dia João,de boas?")
+    println("João:- Na tranquilidade Anderson, quais as novidades? ")
+    println("Anderson:- Assistiu o episódio o novo episódio de Dois Homens e Meio? ")
+    println("João:- Ainda não... como foi?")
+    println("Anderson:- Nossa, melhor episódio você tem que ver imediatamente")
+    println("João:- SEM SPOILERS!")
+    println("Anderson:-Viu que B99 vai acabar?? ")
+    println("João:- Oh não cara!!! Não creio. The big bang Theory lançou o ep final também!")
+    println("Anderson:- Caramba porquê série boa acaba??? ")
+    println("João:- Cara tenho que ir pro trampo agora, vamos marcar pra sair ?")
+    println("Anderson:- Topo... se tiver comida!!")
+    println("João:- Vamos marcando, tô pensando em um Happy Hour ")
+    println("Anderson:- Nossa tô dentro lá na esquina do Zé?  ")
+    println("João:-Lógico com bastante queijo de cabraaa ")
+    println("Anderson:- Me manda por mensagem o horário e o dia ")
+    println("João:- Fechouu, até logo")
+    println("Anderson:- Até!!!")
+    println("E os dois sairam de cena")
+
+
+
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+
+}
