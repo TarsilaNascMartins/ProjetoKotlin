3fca379136ab8d747fbe35027ab0cb33ac0e822b
Tarsila Nascimento Martins <tarsilawho@gmail.com>
2022-02-21 22:39:14 -0300

    Exercicios que  foram dados para converter em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
-
+/*
+// Teste de texto em Kotlin
     println("Anderson:- Bom dia João,de boas?")
     println("João:- Na tranquilidade Anderson, quais as novidades? ")
     println("Anderson:- Assistiu o episódio o novo episódio de Dois Homens e Meio? ")
@@ -17,10 +18,70 @@ fun main() {
     println("Anderson:- Me manda por mensagem o horário e o dia ")
     println("João:- Fechouu, até logo")
     println("Anderson:- Até!!!")
-    println("E os dois sairam de cena")
+    println("E os dois sairam de cena")*/
 
+    /*
+    //Programa para categoria de idade
+    println("SISTEMA DE IDADE DE CLASSIFICAÇÃO PARA NADADOR\n\n")
+    println("Digite sua idade: ")
+    var age = readLine()!!.toInt()
+    println(" Sua idade é de: $age anos")
 
+    if (age < 8) { println("Você está na Liga Infantil A!")
+    } else if (age >= 8 && age < 12) {println("Você está na Liga Infantil B!")
+    }else if (age >= 12 && age < 14) {println("Você está na Liga Juvenil A!")
+    }else if (age >= 14 && age < 18) {println("Você está na Liga Juvenil B!")
+    } else {println ("Você está na Categoria Adulto!")}
+        */
+/*
+    // Teste de Request Answer
+    println("Qual seu nome: ")
+    val nome = readLine()
 
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Qual sua idade: ")
+    val idade = readLine()
 
-}
+    println("Qual sua altura: ")
+    val altura = readLine()
+
+    println("Seu nome é: $nome, Sua idade é: $idade, Sua altura é: $altura")
+*/
+
+/*
+    //Exercício de soma, média e quantidade de valores lidos
+    var num1 = 0.0
+    var soma = 0.0
+    var qtd = 0.0
+
+    while(num1 >= 0.0){
+
+        print("Escolha um número para o programa ")
+        num1 = readLine()!!.toDouble()
+
+        if(num1 > 0.0) {
+            soma = soma + num1
+            qtd++}
+        //escreva("\nnumero = ", num1, " soma parcial = ", soma, " quantidade parcial = ", qtd, "\n")
+    }
+
+    val media : Double = soma / qtd
+    println("A soma dos números digitados é de $soma")
+    println("A média dos números digitados é de: $media \nSendo que foram $qtd valores lidos" )
+*/
+    /*
+    // Exercício para multa no peso de frutas
+    var exc = 0
+    val mult = 0
+
+    print("Digite o Peso: ")
+    val peso = readLine()!!.toInt()
+
+    if ( peso <= 50){
+        print("não haverá multa")
+    }
+    else {
+        val exc: Int = peso - 50
+        val mult: Int = exc * 4
+        print(" Você pagará ,$mult de Multa")
+    }*/
+    }
